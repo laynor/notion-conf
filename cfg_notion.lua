edac3ca9c63aa0435382d8203b8e77368daf98a4
Alessandro Piras <laynor@gmail.com>
2013-03-18 01:35:45 +0100

    update notion config

--- cfg_notion.lua
+++ cfg_notion.lua
@@ -4,6 +4,8 @@ ALTMETA="Mod1+"
 dopath("cfg_defaults")
 dopath("cfg_notioncore")
 dopath("nest_ws.lua")
+dopath("mod_fuzzy_query")
+dopath("emacs_bindings")
 
 defbindings("WScreen", {
    bdoc("Go to next/previous screen on multihead setup."),
@@ -13,6 +15,11 @@ defbindings("WScreen", {
 defbindings("WFrame.toplevel", {
    kpress(META.."O", "WFrame.switch_next(_)"),
    kpress(META.."U", "WFrame.switch_prev(_)"),
+})
+
+defbindings("WMPlex.toplevel", {
    bdoc("Display the main menu."),
    kpress(ALTMETA.."F2", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
+   bdoc("Query for a client window to go to."),
+   kpress(META.."J", "mod_query.query_fuzzy_gotoclient(_)"),
 })
