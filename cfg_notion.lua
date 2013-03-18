f4198313a10f601190d018287a5e34777329f32c
Alessandro Piras <laynor@gmail.com>
2013-03-18 01:47:54 +0100

    unbind ALTMETA..F2 wmplex

--- cfg_notion.lua
+++ cfg_notion.lua
@@ -2,7 +2,6 @@ META="Mod4+"
 ALTMETA="Mod1+"
 
 dopath("cfg_defaults")
-dopath("cfg_notioncore")
 dopath("nest_ws.lua")
 dopath("mod_fuzzy_query")
 dopath("emacs_bindings")
@@ -10,6 +9,8 @@ dopath("emacs_bindings")
 defbindings("WScreen", {
    bdoc("Go to next/previous screen on multihead setup."),
    kpress(META.."Shift+period", "ioncore.goto_next_screen()"),
+   bdoc("Display the main menu."),
+   kpress(ALTMETA.."F2", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
 })
 
 defbindings("WFrame.toplevel", {
@@ -18,8 +19,7 @@ defbindings("WFrame.toplevel", {
 })
 
 defbindings("WMPlex.toplevel", {
-   bdoc("Display the main menu."),
-   kpress(ALTMETA.."F2", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
    bdoc("Query for a client window to go to."),
    kpress(META.."J", "mod_query.query_fuzzy_gotoclient(_)"),
+   kpress(ALTMETA.."F2", nil),
 })
